50358662d7e0935384e5105b255553d95d7dbbad
jguerreiro <joel.borghi@gmail.com>
2022-01-25 10:38:33 -0300

    update - numeros primos

--- busca-binaria.js
+++ busca-binaria.js
@@ -1,5 +1,18 @@
 /* 
-
+• A busca binária é um algoritmo mais eficiente, entretanto,
+requer que a lista esteja ordenada pelos valores da chave de
+busca.
+• A ideia do algoritmo é a seguinte (assuma que a lista está
+ordenada pelos valores da chave de busca):
+• Verifique se a chave de busca é igual ao valor da posição do meio
+da lista.
+• Caso seja igual, devolva esta posição.
+• Caso o valor desta posição seja maior que a chave, então repita o
+processo, mas considere uma lista reduzida, com os elementos do
+começo da lista até a posição anterior a do meio.
+• Caso o valor desta posição seja menor que chave, então repita o
+processo, mas considere uma lista reduzida, com os elementos da
+posição seguinte a do meio até o final da lista.
 */
 
 function buscaBinaria(lista, chave) {
---
+++ busca-sequencial-ordenada.js
@@ -0,0 +1,21 @@
+/* 
+
+Algoritmo de busca sequencial em vetor/array de inteiros ORDENADO -  Autor : Joel Guerreiro
+• A função deve receber uma lista de números inteiros e uma
+chave para busca.
+
+• A função deve retornar o índice da lista que contém a chave ou o
+valor −1, caso a chave não esteja na lista.
+
+*/
+function busca(lista, chave) {
+    for (let indice = 0; indice < lista.length; indice++) {
+        
+    }
+}
+
+const c = 1000;
+const l = [11, 13, 14, 65, 87, 93, 320];
+
+console.log(busca(l, c));
+
