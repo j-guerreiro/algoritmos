31fc9fc0404442700842fa8d478e11f0283eeaa3
jguerreiro <joel.borghi@gmail.com>
2022-01-27 12:03:48 -0300

    update

--- alice-n-bob.js
+++ alice-n-bob.js
@@ -9,16 +9,12 @@ Write a program that only the users Alice and Bob are greeted with their names.
 import entrada from "readline-sync";
 
 function isAliceOrBob( user ) {
-
     if ( user == "Alice" || user == "Bob" ) {
         return "\nHello " + user + "!\n";
     } else {
         return "\nUnathorized User!\n";
     }
 }
+let usuario = entrada.question("\nuser: ");
+console.log(isAliceOrBob(usuario));
 
-usuario = entrada.question("user: ");
-
-callFunc = isAliceOrBob(usuario);
-
-console.log(callFunc);
