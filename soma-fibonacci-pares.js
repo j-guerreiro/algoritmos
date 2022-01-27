3b56eb304eb2321a03ee580ab66d973134049b58
jguerreiro <joel.borghi@gmail.com>
2022-01-27 17:09:05 -0300

    update

--- soma-fibonacci-pares.js
+++ soma-fibonacci-pares.js
@@ -7,9 +7,6 @@ Ao iniciar a sequencia por 1 e 2, os 10 primeiros termos serao:
 
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
-By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
-find the sum of the even-valued terms.
-
 Considerando os termos na Fibonacci cujos valores nao excedam 4 milhoes,
 encontre a soma dos termos pares.
 
