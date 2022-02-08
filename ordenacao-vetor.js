0009e9f22435774e77b4260861a1513ec4d65937
j-guerreiro <joel.borghi@gmail.com>
2022-02-07 22:32:09 -0300

    update - ordenacao de vetor depurado - ok

--- ordenacao-vetor.js
+++ ordenacao-vetor.js
@@ -2,14 +2,23 @@
 
 # Autor da solucao: Joel Guerreiro
 
+Algoritmo básico de ordenação de vetor.
 
 */
-
-function ordenaVetor( vetor) {
-
+function ordenaVetor( vetor ) {
+  let aux = 0;
+  let aux2 = 0;
+  for ( let i = 0 ; i < vetor.length ; i ++) {
+    for ( let i = 0 ; i < vetor.length ; i ++) {
+      if ( vetor[ i ] > vetor[ i + 1 ] ) {
+        aux = vetor[ i + 1];
+        aux2 = vetor[ i ];
+        vetor[ i ] = aux;
+        vetor[ i + 1] = aux2;
+      }
+    }
+  }
+  console.log(vetor.join(", "));
 }
-
-
-v = [ 10, 8, 2, 7, 3, 6, 15, 3];
-
+let v = [ 10, 100, 8, 2, 5, 1, 12, 50, 3];
 ordenaVetor( v );
