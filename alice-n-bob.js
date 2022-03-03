ff0ca72122564d3faeba8a7a7bfe416207df37f8
jguerreiro <joel.borghi@gmail.com>
2022-03-03 19:11:25 -0300

    update - template literals JavaScript Style Guides

--- alice-n-bob.js
+++ alice-n-bob.js
@@ -8,13 +8,16 @@ Write a program that only the users Alice and Bob are greeted with their names.
 
 import entrada from "readline-sync";
 
-function isAliceOrBob( user ) {
-    if ( user == "Alice" || user == "Bob" ) {
-        return "\nHello " + user + "!\n";
-    } else {
-        return "\nUnathorized User!\n";
-    }
+function isAliceOrBob(user) {
+  let message = ``;
+  if (user === "Alice" || user === "Bob") {
+    message = `\nHello ${user}, welcome back!\n`;
+    return message;
+  } else {
+    message = `\nUnathorized User!\n`;
+    return message;
+  }
 }
-let usuario = entrada.question("\nuser: ");
+let usuario = entrada.question("\nUser: ");
 console.log(isAliceOrBob(usuario));
 
