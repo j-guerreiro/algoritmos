4c03cc97da525c3a5d31ff0c147853f72b03a57a
jguerreiro <joel.borghi@gmail.com>
2022-03-03 17:17:20 -0300

    update - added condicao e funcao date ficou externa

--- signo-de-hoje.js
+++ signo-de-hoje.js
@@ -2,7 +2,7 @@
 
 # Programa da DevMedia
 
-O programa calcula o signo do dia
+O programa calcula o signos do dia
 
 Conceitos abordados:
 
@@ -27,14 +27,28 @@ let colecao_signos = [
 ];
 
 
-const retorna_signos = (signo) => {
-  //acessa o elemento da colecao via indice/posicao e chave do objeto
-  let mensagem = "\nSigno de hoje: " + signo[0]["Nome"] + "\n"
-  return mensagem;
+const retorna_signos = (signos, data) => {
+
+  //atribuindo somente o ano da data a variavel ano 
+  let ano = data.getFullYear();
+
+  let data_inicio_signo = new Date(ano + "-" + signos[0]["DataInicio"] + " 00:00:00");
+  let data_fim_signo = new Date(ano + "-" + signos[0]["DataFim"] + " 23:59:59");
+
+  if (data >= data_inicio_signo && data <= data_fim_signo) {
+    //acessa o elemento da colecao via indice/posicao e chave do objeto
+    let mensagem = "\nSigno de hoje: " + signos[0]["Nome"] + "\n"
+    return mensagem;
+  }
+
+
 }
 
+//instanciando um novo objeto data
+let data_app = new Date("2022-02-07 00:00:00");
+
 //atribuindo a funcao a uma constante
-const nome_signo = retorna_signos(colecao_signos);
+const nome_signos = retorna_signos(colecao_signos, data_app);
 
 //escreve o valor na saida padrao
-console.log(nome_signo);
+console.log(nome_signos);
