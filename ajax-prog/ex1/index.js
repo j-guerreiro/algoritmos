dace9ade9706a6455f500f53cab0c40d622b812a
j-guerreiro <joel.borghi@gmail.com>
2022-03-06 18:28:39 -0300

    update - added more code

---
+++ ajax-prog/ex1/index.js
@@ -0,0 +1,24 @@
+const mostrarPaises = () => {
+  
+  let xhr = new XMLHttpRequest();
+  
+  xhr.open('GET', 'https://restcountries.com/v3.1/region/ame', true);
+  
+  xhr.onload = () => {
+
+    if ( xhr.status == 200) {
+      let paises = JSON.parse(response);
+      paises.forEach(pais => {
+
+        const cardPais = document.createElement('div');
+        const bandeiraPais = document.createElement('img');
+        cardPais.innerHTML = pais.name;
+        bandeiraPais.src = pais.flag;
+        cardPais.appendChild(bandeiraPais);
+        document.getElementById('post').appendChild(cardPais);
+      
+      });
+    }
+  }
+  xhr.send();
+}
