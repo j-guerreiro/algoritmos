069611f3226775af674106fce85e5a5ee19f6d45
jguerreiro <joel.borghi@gmail.com>
2022-03-16 13:19:06 -0300

    update - criando subrotinas e delegando funcoes e responsabilidades para elas

--- calculadora-de-conversoes.js
+++ calculadora-de-conversoes.js
@@ -17,12 +17,74 @@ import entrada from 'readline-sync';
 //1
 const celsiusParaFahrenheit = (celsius) => {
 
+  console.log("Celsius para Fahrenheit");
+  let resultado = celsius
+
 }
 //2
 const fahrenheitParaCelsius = (fahrenheit) => {
 
 }
 //3
-const
+const celsiusParaKelvin = (celsius) => {
 
+}
 //4
+const kelvinParaCelsius = (kelvin) => {
+
+}
+//5
+const fahrenheitParaKelvin = (fahrenheit) => {
+
+}
+//5
+const kelvinParaFahrenheit = (kelvin) => {
+
+}
+//6
+const kmParaMilhas = (km) => {
+
+}
+//7
+const milhasParaKm = (milhas) => {
+
+}
+//opcoes
+const opcoes = () => {
+  conversoes = [
+    { opcao: "(1) Celsius para Fahrenheit" },
+    { opcao: "(2) Fahrenheit para Celsius" },
+    { opcao: "(3) Celsius para Kelvin" },
+    { opcao: "(4) Kelvin para Celsius" },
+    { opcao: "(5) Fahrenheit para Kelvin" },
+    { opcao: "(6) Kelvin para Fahrenheit" },
+    { opcao: "(7) Quilômetros para Milhas" },
+    { opcao: "(8) Milhas para Quilômetros" },
+    { opcao: "(9) Sair" },
+  ]
+
+  for (const opcao of conversoes) {
+    console.log(opcao)
+  }
+}
+
+const escolhaCaso = (opcaoEscolhida) => {
+  switch (opcaoEscolhida) {
+    case 1:
+      let valor = Number(entrada.question("Entre com o valor: "));
+      console.log(celsiusParaFahrenheit(valor))
+      break;
+
+    default:
+      break;
+  }
+}
+
+do {
+  console.log("Calculadora de Múltiplas Conversões");
+  console.log(opcoes());
+  let opcao = opcao.question("Escolha uma opção: ");
+  console.log(escolhaCaso(opcao))
+
+} while (opcao != 8)
+
