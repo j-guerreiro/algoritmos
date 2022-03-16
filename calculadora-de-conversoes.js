f8b0044ddee399c69ea7d1039c3b947becfbc626
jguerreiro <joel.borghi@gmail.com>
2022-03-16 15:11:32 -0300

    update - testando e refatorando o codigo

--- calculadora-de-conversoes.js
+++ calculadora-de-conversoes.js
@@ -17,78 +17,103 @@ import entrada from 'readline-sync';
 //1
 const celsiusParaFahrenheit = (celsius) => {
 
-  console.log("Celsius para Fahrenheit");
-  let conversao = (celsius * 1.8) + 32;
-  let resultado = `Celsius ${celsius} => Fahrenheit: ${conversao}`;
-  return resultado;
+  console.log("\n   ->Celsius para Fahrenheit<-   ");
+  console.log("<=========== RESULTADO ===========>");
+  let conversao = ((celsius * 1.8) + 32).toFixed(2);
+  let resultado = `\n | ${celsius}°C equivalem a => ${conversao}°F | \n`;
+  console.log(resultado);
+  console.log("<=========== RESULTADO ===========>\n\n");
 
 }
 //2
 const fahrenheitParaCelsius = (fahrenheit) => {
 
-  console.log("Fahrenheit para Celsius");
-  let conversao = (fahrenheit - 32) / 1.8;
-  let resultado = `Fahrenheit ${fahrenheit} => Celsius: ${conversao}`;
-  return resultado;
+  console.log("\n   ->Fahrenheit para Celsius<-   ");
+  console.log("<=========== RESULTADO ===========>");
+  let conversao = ((fahrenheit - 32) / 1.8).toFixed(2);
+  let resultado = `\n | ${fahrenheit}°F equivalem a => ${conversao}°C | \n`;
+  console.log(resultado);
+  console.log("<=========== RESULTADO ===========>\n\n");
 
 }
 //3
 const celsiusParaKelvin = (celsius) => {
 
-  console.log("Celsius para Kelvin");
-  let conversao = (celsius + 273.15);
-  let resultado = `Celsius ${celsius} => Fahrenheit: ${conversao}`;
-  return resultado;
+  console.log("\n   ->Celsius para Kelvin<-   ");
+  console.log("<=========== RESULTADO ===========>");
+  let conversao = (celsius + 273.15).toFixed(2);
+  let resultado = `\n ${celsius}°C equivalem a => ${conversao}°F\n`;
+  console.log(resultado);
+  console.log("<=========== RESULTADO ===========>\n\n");
+
 
 }
 //4
 const kelvinParaCelsius = (kelvin) => {
 
-  console.log("Celsius para Celsius");
-  let conversao = (kelvin - 273.15);
-  let resultado = `Kelvin ${kelvin} => Celsius: ${conversao}`;
-  return resultado;
+  console.log("\n   ->Kelvin para Celsius<-   ");
+  console.log("<=========== RESULTADO ===========>");
+  let conversao = (kelvin - 273.15).toFixed(2);
+  let resultado = `\n ${kelvin}°K equivalem a => ${conversao}°C\n`;
+  console.log(resultado);
+  console.log("<=========== RESULTADO ===========>\n\n");
+
 
 }
 //5
 const fahrenheitParaKelvin = (fahrenheit) => {
 
-  console.log("Fahrenheit para Kelvin");
-  let conversao = ((fahrenheit - 32) * 1.8) + 273.15;
-  let resultado = `Fahrenheit ${fahrenheit} => Kelvin: ${conversao}`;
-  return resultado;
+  console.log("\n   ->Fahrenheit para Kelvin<-   ");
+  console.log("<=========== RESULTADO ===========>");
+  let conversao = (((fahrenheit - 32) * 1.8) + 273.15).toFixed(2);
+  let resultado = `\n ${fahrenheit}°F equivalem a => ${conversao}°K\n`;
+  console.log(resultado);
+  console.log("<=========== RESULTADO ===========>\n\n");
+
 
 }
 //6
 const kelvinParaFahrenheit = (kelvin) => {
 
-  console.log("Kelvin para Fahrenheit");
-  let conversao = ((kelvin - 273.15) * 1.8) + 32;
-  let resultado = `Kelvin ${kelvin} => Fahrenheit: ${conversao}`;
-  return resultado;
+  console.log("\n   ->Kelvin para Fahrenheit<-   ");
+  console.log("<=========== RESULTADO ===========>");
+  let conversao = (((kelvin - 273.15) * 1.8) + 32).toFixed(2);
+  let resultado = `\n ${kelvin}°K equivalem a => ${conversao}°F\n`;
+  console.log(resultado);
+  console.log("<=========== RESULTADO ===========>\n\n");
+
 
 }
 //7
 const kmParaMilhas = (km) => {
 
-  console.log("Quilômetros para Milhas");
-  let conversao = km / 1.609;
-  let resultado = `Quilômetros ${km} => Milhas: ${conversao}`;
-  return resultado;
+  console.log("\n       ->Km/h para Mp/h<-   ");
+  console.log("<=========== RESULTADO ===========>");
+  let conversao = (km / 1.609).toFixed(2);
+  let resultado = `\n ${km}Km/h equivalem a => ${conversao}Mp/h\n`;
+  console.log(resultado);
+  console.log("<=========== RESULTADO ===========>\n\n");
+
 
 }
 //8
 const milhasParaKm = (milhas) => {
 
-  console.log("Quilômetros para Milhas");
+  console.log("\n       ->Mp/h para Km/h<-   ");
+  console.log("<=========== RESULTADO ===========>");
   let conversao = milhas * 1.609;
-  let resultado = `Milhas ${km} => Quilômetros: ${conversao}`;
-  return resultado;
+  let resultado = `\n ${km}Mph equivalem a => ${conversao}Km/h`;
+  console.log(resultado);
+  console.log("<=========== RESULTADO ===========>\n\n");
 
 }
 //opcoes
 const opcoes = () => {
   const opcoes = [
+    { chave: "<=Calculadora de Múltiplas Conversões=>" },
+    { chave: "                                     " },
+    { chave: "    Selecione uma opção de 1 a 9     " },
+    { chave: "                                     " },
     { chave: "<==== GRANDEZAS DE TEMPERATURAS ====>" },
     { chave: "                                     " },
     { chave: "(1) Celsius para Fahrenheit - (C) => (F)" },
@@ -98,12 +123,13 @@ const opcoes = () => {
     { chave: "(5) Fahrenheit para Kelvin  - (F) => (K)" },
     { chave: "(6) Kelvin para Fahrenheit  - (K) => (F)" },
     { chave: "                                        " },
-    { chave: "<==== GRANDEZAS DE COMPRIMENTO ====>" },
+    { chave: "<==== GRANDEZAS DE VELOCIDADE ====>" },
     { chave: "                                      " },
-    { chave: "(7) Quilômetros para Milhas - (KM)=> (M)" },
-    { chave: "(8) Milhas para Quilômetros - (M) => (KM)" },
+    { chave: "(7) Quilômetros para Milhas - (Km/h)=> (Mph)" },
+    { chave: "(8) Milhas para Quilômetros - (Mph) => (Km/h)" },
     { chave: "                                      " },
     { chave: "<=" },
+    { chave: "                                      " },
     { chave: "(9) Sair" },
     { chave: "                                      " },
   ];
@@ -118,42 +144,42 @@ const escolhaCaso = (opcaoEscolhida) => {
   switch (opcaoEscolhida) {
     case 1:
       valor = Number(entrada.question("Entre com o valor: "));
-      console.log(celsiusParaFahrenheit(valor));
+      celsiusParaFahrenheit(valor);
       break;
 
     case 2:
       valor = Number(entrada.question("Entre com o valor: "));
-      console.log(fahrenheitParaCelsius(valor));
+      fahrenheitParaCelsius(valor);
       break;
 
     case 3:
       valor = Number(entrada.question("Entre com o valor: "));
-      console.log(celsiusParaKelvin(valor));
+      celsiusParaKelvin(valor);
       break;
 
     case 4:
       valor = Number(entrada.question("Entre com o valor: "));
-      console.log(kelvinParaCelsius(valor));
+      kelvinParaCelsius(valor);
       break;
 
     case 5:
       valor = Number(entrada.question("Entre com o valor: "));
-      console.log(fahrenheitParaKelvin(valor));
+      fahrenheitParaKelvin(valor);
       break;
 
     case 6:
       valor = Number(entrada.question("Entre com o valor: "));
-      console.log(kelvinParaFahrenheit(valor));
+      kelvinParaFahrenheit(valor);
       break;
 
     case 7:
       valor = Number(entrada.question("Entre com o valor: "));
-      console.log(kmParaMilhas(valor));
+      kmParaMilhas(valor);
       break;
 
     case 8:
       valor = Number(entrada.question("Entre com o valor: "));
-      console.log(milhasParaKm(valor));
+      milhasParaKm(valor);
       break;
 
     case 9:
@@ -165,11 +191,10 @@ const escolhaCaso = (opcaoEscolhida) => {
   }
 }
 
-do {
-  console.log("\nCalculadora de Múltiplas Conversões\n");
+let opcao;
+
+while (opcao != 9) {
   opcoes();
-  let opcao = Number(entrada.question("Escolha uma opção: "));
+  opcao = Number(entrada.question("Escolha uma opção: "));
   escolhaCaso(opcao);
-
-} while (opcao != 9)
-
+}
