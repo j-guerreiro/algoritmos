a8301b76cfc1ba8839c1a1dbb47abedf588e2a0b
jguerreiro <joel.borghi@gmail.com>
2022-03-16 12:50:58 -0300

    update - criando calculadora de multiplas conversoes

---
+++ calculadora-de-conversoes.js
@@ -0,0 +1,28 @@
+/*
+
+Autor: Joel Guerreiro
+
+Calculadora de conversoes de valores.
+
+1. Selecionar a opcao
+2. Entrar com o valor a ser convertido
+3. Apresentar o resultado da conversao
+4. Enquanto a opcao nao for sair, apresentar opcoes de conversao, 
+senao, encerrar o programa.
+
+*/
+
+import entrada from 'readline-sync';
+
+//1
+const celsiusParaFahrenheit = (celsius) => {
+
+}
+//2
+const fahrenheitParaCelsius = (fahrenheit) => {
+
+}
+//3
+const
+
+//4
