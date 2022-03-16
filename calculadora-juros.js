d6318d19d128a5dd3cccde45b92babdf2322d77d
jguerreiro <joel.borghi@gmail.com>
2022-03-16 17:55:52 -0300

    update - refatorando com condicional

--- calculadora-juros.js
+++ calculadora-juros.js
@@ -15,16 +15,19 @@ import entrada from 'readline-sync';
 
 const calcularJuros = (valor, dias) => {
 
-  let juros = (dias > 15 && valor > 0) ? valor * 0.10 : valor * 0.05;
+  if (valor > 0) {
+    let juros = (dias > 15) ? valor * 0.10 : valor * 0.05;
 
-  juros = Number(parseInt(juros));
+    juros = Number(parseInt(juros));
 
-  let montante = Number(parseInt(valor + juros)).toFixed(2);
+    let montante = Number(parseInt(valor + juros)).toFixed(2);
 
-  let resultado = `\n\tPrezado, \n\n\t O atraso de ${dias} dias no valor de R$ ${valor.toFixed(2)} \n\n\tfoi acrescido de juros de R$ ${juros} e o valor total com juros é de: R$ ${montante} . \n`;
+    let resultado = `\n\tPrezado, \n\n\t O atraso de ${dias} dias no valor de R$ ${valor.toFixed(2)} \n\n\tfoi acrescido de juros de R$ ${juros} e o valor total com juros é de: R$ ${montante} . \n`;
 
-  return resultado;
-
+    return resultado;
+  }
+  let msg = `Valor inválido`;
+  return msg;
 }
 
 const valorPagamento = Number(entrada.question("\nInforme o valor devido em R$:...... "));
