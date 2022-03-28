4f85d47affe3c33bc87ad37977fb9e9c1773fea0
j-guerreiro <joel.borghi@gmail.com>
2022-03-27 23:30:52 -0300

    update - refatorando

--- funcao-substring.js
+++ funcao-substring.js
@@ -2,11 +2,11 @@
  * Autor: Joel Guerreiro
  * Algoritmo que faz a troca de uma substring de uma string.
  * */
-function trocaString( stringTexto ) {
-  let textoNovo = stringTexto.replace( stringTexto.substr( 10, 10 ), "JS" );
+function trocaString(stringTexto) {
+  let textoNovo = stringTexto.replace( stringTexto.substr( 10, 10 ),"JS");
   return textoNovo;
 }
 
 const varTexto = "\nAprender JavaScript é gratificante!\nA linguagem JavaScript é bastante requisitada nas empresas, \ntanto no Brasil como nos países estrangeiros!\n";
 
-console.log( trocaString( varTexto ) );
+console.log(trocaString(varTexto));
