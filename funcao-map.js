720bdd84a4d2fa4f1152a0c484c015fd5484b88f
jguerreiro <joel.borghi@gmail.com>
2022-03-30 15:43:07 -0300

    update - refatorando

--- funcao-map.js
+++ funcao-map.js
@@ -3,30 +3,25 @@
 Autor: Joel Guerreiro
 
 Estudo de função nativa 'map()' de arrays do JavaScript
-O array de objetos contém informações populacionais de cidades da Estônia
+A funcao map cria um novo array a partir do original
 
 */
-
 const cidadesDaEstonia = [
-  { nome: "Rapla", pais: "Estônia", populacao: 13193, regiao: "Rapla é uma cidade na regiao central da Estônia" },
+  { nome: "Rapla", pais: "Estônia", populacao: 13193, regiao: "Rapla é uma cidade na regiao central da Estônia." },
   { nome: "Tallinn", pais: "Estônia", populacao: 430805, regiao: "Tallin está situada na costa sul do golfo da Finlândia, no centro-norte da Estónia." },
   { nome: "Viljandi", pais: "Estônia", populacao: 17407, regiao: "Viljandi é um município urbano estoniano, que ocupa uma área de 14,6 km² do planalto de Sakala, no sul do país." },
   { nome: "Tartu", pais: "Estônia", populacao: 98449, regiao: "Tartu é a segunda maior cidade da Estônia e capital da região de Tartu." },
   { nome: "Rakvere", pais: "Estônia", populacao: 15526, regiao: "Rakvere é um município urbano estoniano localizado na região de Lääne-Virumaa." },
 ];
 
-const imprimirCidades = (arrayCidades) => {
-
-  const nomePopulacao = {
+const imprimirInformacoes = (arrayCidades) => {
+  const informacoesExtras = {
     nome: arrayCidades.nome,
-    populacao: arrayCidades.populacao,
     regiao: arrayCidades.regiao,
   }
 
-
-
-  // let infoCidades = `\nNome da cidade: ${nomeCidade} \n População: ${populacaoCidade} habitantes\n`;
-  // console.log(infoCidades);
+  let msg = `\nNome da cidade: ${informacoesExtras.nome} \n Região Administrativa: ${informacoesExtras.regiao}\n`;
+  console.log(msg);
 }
 
-const cidades
+const informacoesExtras = cidadesDaEstonia.map(imprimirInformacoes);
